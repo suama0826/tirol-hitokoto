504fc736693e0abad7e3008779de2eef101e7c96
suama0826 <37920042+suama0826@users.noreply.github.com>
2018-04-24 00:06:31 +0900

    Update tirol-hitokoto.js

--- tirol-hitokoto.js
+++ tirol-hitokoto.js
@@ -5,16 +5,16 @@
     var results = document.getElementById('unsei-text');
 
     btn.addEventListener('click', function() {
-        var unsei = ['「肩にわたしのフンが付いたままよ」',
+        var unsei = ['「あら、肩にわたしのフンが付いたままよ」',
                      '「私のあさごはん忘れてない？」',
                      '「ぷんすかぷんすか！キャルルルル！」',
-                     '「はやく、粟穂をよこしなさい!」',
+                     '「はやく、粟穂をよこしなさい」',
                      '「今夜は甘えて良いかしら。。。」',
                      '「誰がおまんじゅうやねん！💢」',
                      '「午後8時には眠りたいわ」',
-                     '「とにかくはよせえ」',
+                     '「遅かったじゃない。どこへ出かけてたの」',
                      '「誕生日は4月12日よ。よろしく」',
-                     '「ラムネくんのことは覚えているわ」'];
+                     '「甘噛みしてあげるわ」'];
         var n = Math.floor(Math.random() * unsei.length);
         results.textContent = unsei[n];
     });
